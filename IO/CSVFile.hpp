1d9dbe1a9e739172ae297a85132cdea9158cc221
Jack Hunt <jackmileshunt@gmail.com>
2018-07-30 01:54:12 +0100

    More work on CSVFile.

--- IO/CSVFile.hpp
+++ IO/CSVFile.hpp
@@ -37,12 +37,23 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <variant>
 #include <vector>
 #include <memory>
+#include <utility>
+#include <algorithm>
 #include <fstream>
-#include <sstream>
+#include <iostream>
+
+#include "../StringManipulation/Tokenizing.hpp"
 
 namespace CPPUtils::IO {
     template<typename R, typename I>
     class CSVFile {
+    protected:
+        // Type of each column. Length determines number of columns.
+        std::vector<TokenTypes> types;
+
+        // CSV lines stored here.
+        std::vector<CSVRow> data;
+
     public:
         // Parseable token types.
         enum class TokenType : short {
@@ -50,17 +61,64 @@ namespace CPPUtils::IO {
             INTEGER,
             BOOLEAN,
             STRING
+        };
+
+        // Elements can be Real, Integer, Boolean or String.
+        using CSVElement = std::variant<R, I, bool, std::string>; 
+        using CSVRow = std::vector<CSVElement>;
+
+      protected:
+        CSVRow parseTokens(const std::vector<std::string> &tokens) {
+            if (types.size() != 0) {
+                assert(tokens.size() == types.size());
+            }
+
+            // For each token, find it's type and add.
+            CSVRow row;
+            std::vector<TokenTypes> parsedTypes;
+            for (const auto &token : tokens) {
+                // First try floating point.
+                try {
+                    const R val = static_cast<R>(std::stold(token));
+                    row.push_back(CSVElement({val}));
+                    parsedTypes.push_back(TokenType::REAL);
+                    continue;
+                } catch(const std::invalid_argument &e) {}
+
+                // Next try integer.
+                try {
+                    const I val = static_cast<I>(std::stoll(token));
+                    row.push_back(CSVElement({val}));
+                    parsedTypes.push_back(TokenType::INTEGER);
+                    continue;
+                } catch (const std::invalid_argument &e) {}
+
+                // Then boolean.
+                if (token == "True" || token == "true" || token == "T") {
+                    row.push_back(CSVElement({true}));
+                    parsedTypes.push_back(TokenType::BOOLEAN);
+                    continue;
+                }
+                if (token == "False" || token == "false" || token == "F") {
+                    row.push_back(CSVElement({false}));
+                    parsedTypes.push_back(TokenType::BOOLEAN); 
+                    continue;
+                }
+
+                // If we get here, it's not parsable, so place it in verbatim.
+                row.push_back(token);
+                parsedTypes.push_back(TokenType::STRING);
+            }
+
+            // Verify the types and return.
+            if (types.size() == 0) { // If first parse and types not set.
+                types = parsedTypes;
+            } 
+            else {
+                assert(std::equal(types.begin(), types.end(), parsedTypes.begin()));
+            }
+            return row;
         }
-
-        // Row type.
-        using CSVRow = std::variant<R, I, bool, std::string>;
-
-    private:
-        // Type of each column. Length determines number of columns.
-        std::vector<TokenTypes> types;
-
-        // CSV lines stored here.
-        std::vector<CSVRow> data;
 
     public:
         CSVFile() {
@@ -76,15 +134,57 @@ namespace CPPUtils::IO {
         }
         
         void readFile(const std::string &fileName) {
-            // Make input stream.
-            std::unique_ptr<std::ifstream> inStr(new std::ifstream(), 
-                                                 [](std::ifstream *s) { s->close(); });
+            // Import delimiter tokenizing routine.
+            using CPPUtils::StringManipulation::splitOnDelimiter();
+
+            // Make input stream and attempt to open given file.
+            try {
+                std::unique_ptr<std::ifstream> inStr(new std::ifstream(fileName), 
+                                                     [](std::ifstream *s) { 
+                                                         if (s->is_open()) {
+                                                             s->close();
+                                                         }
+                                                         delete s; 
+                                                     });
+            }
+            catch (const std::ifstream::failure &e) {
+                std::cerr << "CSV: Error opening input file: " << fileName << std::endl;
+                throw e;
+            }
+
+            // Read each line.
+            std::string line;
+            while (std::getline(*inStr.get(), line)) {
+                // Split the line and verify correct size.
+                const auto tokens = splitOnDelimiter(line, ',');
+                const auto row = parseTokens(tokens);
+                data.push_back(row);
+            }
         }
 
         void writeFile(const std::string &fileName) const {
+            // If nothing to write, early out.
+            if (data.size() == 0) {
+                return;
+            }
+
             // Make output stream.
-            std::unique_ptr<std::ofstream> inStr(new std::ofstream(),
-                                                 [](std::ifstream *s) { s->close(); });
+            std::unique_ptr<std::ofstream> outStr(new std::ofstream(fileName),
+                                                  [](std::ofstream *s) { 
+                                                      if (s->is_open()) {
+                                                          s->close();
+                                                      }
+                                                      delete s;
+                                                  });
+
+            // Write out each row.
+            for (const auto &row : data) {
+                std::string line;
+                std::stringstream lineStream(outString);
+                for (const auto &elem : row) {
+                    //lineStream << elem.
+                }
+            }
         }
 
         void appendRow(const std::string &row) {
@@ -104,6 +204,10 @@ namespace CPPUtils::IO {
         }
 
         std::vector< std::vector<R> > getDataNumeric() const {
+            //
+        }
+
+        std::vector<std::string> getDataString() const {
             //
         }
 
