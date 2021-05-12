38e4f1f46b1c8e82c42459bbdcca498e889a1509
Jack Hunt <jackmileshunt@gmail.com>
2021-05-12 22:58:58 +0100

    Add simple CSV test.

--- Tests/IO/CSVFile.cpp
+++ Tests/IO/CSVFile.cpp
@@ -32,6 +32,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #define BOOST_TEST_MODULE CSVFileTests
 
+#include <filesystem>
+
 #include <boost/test/included/unit_test.hpp>
 
 #include <CPPUtils/IO/CSVFile.hpp>
@@ -41,7 +43,49 @@ using namespace CPPUtils::IO;
 BOOST_AUTO_TEST_SUITE(CSVFileTestSuite)
 
 BOOST_AUTO_TEST_CASE(BasicCSVWriteTest) {
-    //
+    // Test data.
+    constexpr auto a = "3.14, True, 2, 6.28, abc";
+    constexpr auto b = "6.28, False, -2, 3.14, cba";
+
+    // Create a CSV file and append some data.
+    CSVFile<float, int> csv;
+    csv.appendRow(a);
+    csv.appendRow(b);
+
+    // Get temp directory path.
+    const auto tmp_path = std::filesystem::temp_directory_path();
+    const auto fname = tmp_path / "test.csv";
+
+    // Write the CSV out to a file.
+    csv.writeToDisk(fname.string());
+
+    // Read the file back in.
+    decltype(csv) csv2;
+    csv2.readFromDisk(fname.string());
+
+    // Verify that the types match.
+    const auto types = csv.getDataTypes();
+    const auto types2 = csv2.getDataTypes();
+    BOOST_CHECK_EQUAL_COLLECTIONS(types.cbegin(), types.cend(),
+                                  types2.cbegin(), types2.cend());
+
+    // Verify the rows.
+    const auto rows = csv.getData();
+    const auto rows2 = csv2.getData();
+
+    BOOST_CHECK_EQUAL(rows.size(), rows2.size());
+    BOOST_CHECK_EQUAL(csv.getNumRows(), csv2.getNumRows());
+
+    for (size_t i = 0; i < rows.size(); i++) {
+        const auto rowA = rows.at(i);
+        const auto rowB = rows2.at(i);
+
+        BOOST_CHECK_EQUAL_COLLECTIONS(rowA.cbegin(), rowA.cend(),
+                                      rowB.cbegin(), rowB.cend());
+    }
+
+    // Clear up.
+    BOOST_CHECK(std::filesystem::remove(fname));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
